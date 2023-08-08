fbc4738b8e2950b0071f75136ba779d9a9b87082
sam <samuel6249ng@gmail.com>
2023-08-08 19:57:02 +1000

    bg and tilt

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
+import ParticlesBg from 'particles-bg'
+import React, { Component } from 'react'
+
+//import Tilt from 'react-parallax-tilt';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <ParticlesBg type="cobweb" bg={true} />
+        <ParticlesBg type="cobweb" bg={true} />
+
+        
+
     </div>
   );
 }
